8cf1e23e74b5684101407dca1c06608436da3618
Roger <39733923+roger-n@users.noreply.github.com>
2018-10-04 23:24:10 -0400

    successful spring and thymeleaf setup

---
+++ src/main/java/com/rogern/Example.java
@@ -0,0 +1,15 @@
+package com.rogern;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example {
+
+  
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+
+}
---
+++ src/main/java/com/rogern/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.rogern.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String hi() {
+        return "NewFile";
+    }
+
+    @RequestMapping("add")
+    public String hello(@RequestParam("t1") int i, @RequestParam("t2") int j) {
+        return (i+j)+"";
+    }
+}
